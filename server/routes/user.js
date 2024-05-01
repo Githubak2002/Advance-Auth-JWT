f35e674694e4eaabff68c2d8fbb8e71fdc7e9e8c
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-05-02 01:51:06 +0530

    login done

--- server/routes/user.js
+++ server/routes/user.js
@@ -12,47 +12,9 @@ const router = express.Router();
 router.post("/signup", SignUpController);
 
 // ===== login an existing user ========= JWT
-// router.post("/login", LoginController)
+router.post("/login", LoginController)
 
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await UserModel.findOne({ email });
-    if (!user) {
-      return res.status(404).json({
-        status: false,
-        msg: "User not registered",
-      });
-    }
-    const validPass = await bcrypt.compare(password, user.password);
-    if (!validPass) {
-      return res.status(404).json({
-        status: false,
-        msg: "Email or password is incorrect",
-      });
-    }
-    const token = jwt.sign(
-      { userName: user.userName },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
-    // httpOnly:true - prevent the token from being accessed by client-side scripts
-    // res.cookie("token", token, { httpOnly: true, maxAge: 360000 });
-    res.cookie("token", token, {  maxAge: 360000 });
-    return res.json({
-      status: true,
-      msg: "Login successfully",
-    });
-  } catch (error) {
-    console.log(`Error in login user route → ${error}}`);
-    return res.status(500).json({
-      status: false,
-      msg: "Internal server error",
-    });
-  }
-});
-
-// ===== forgot Password =========
+// ====== Forgot Password =========
 router.post("/forgotPass", async (req, res) => {
   const { email } = req.body;
   try {
